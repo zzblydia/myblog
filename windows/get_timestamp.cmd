bd494c46b09479137a0506d9c934e03821e00e81
zzblydia <13098426+zzblydia@users.noreply.github.com>
2024-05-03 05:16:47 +0800

    format command timestamp style

--- windows/get_timestamp.cmd
+++ windows/get_timestamp.cmd
@@ -28,6 +28,10 @@ rem 方法二
 rem time 0到11点时小时数值前有空格
 echo timestamp_simple:%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 
-if %time:~0,2% LEQ 9 (echo timestamp_simple:%date:~0,4%%date:~5,2%%date:~8,2%_0%time:~1,1%%time:~3,2%%time:~6,2%) else (echo timestamp_simple:%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%)
+if %time:~0,2% LEQ 9 (
+    echo timestamp_simple:%date:~0,4%%date:~5,2%%date:~8,2%_0%time:~1,1%%time:~3,2%%time:~6,2%
+) else (
+    echo timestamp_simple:%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+)
 
 pause
--- windows/ping/start_ping_gateway.cmd
+++ windows/ping/start_ping_gateway.cmd
@@ -1,3 +1,7 @@
-if %time:~0,2% LEQ 9 (set timestamp=%date:~0,4%%date:~5,2%%date:~8,2%_0%time:~1,1%%time:~3,2%%time:~6,2%) else (set timestamp=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%) 
+if %time:~0,2% LEQ 9 (
+    set timestamp=%date:~0,4%%date:~5,2%%date:~8,2%_0%time:~1,1%%time:~3,2%%time:~6,2%
+) else (
+    set timestamp=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+)
 
 cscript C:\vm\ping\ping.vbs 192.168.8.1 -t -l 1000 -w 5000 >>C:\vm\ping\pinglog\%timestamp%_ping_gateway.txt
