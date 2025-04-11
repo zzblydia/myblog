783ce4f2f763571ee9b076193f9a5580c9233baa
zzblydia <13098426+zzblydia@users.noreply.github.com>
2025-04-11 17:30:59 +0800

    Create ebpf_hello.c

---
+++ linux/ebpf/ebpf_hello.c
@@ -0,0 +1,44 @@
+#include <linux/bpf.h>
+#include <linux/pkt_cls.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("classifier")
+int ebpf_hello(struct __sk_buff *skb) {
+    static __u16 count = 0;
+    count++;
+    if (count < 10) {
+        bpf_printk("ebpf_hello %d", count);
+    }
+    return TC_ACT_OK;
+}
+
+char _license[] SEC("license") = "GPL";
+
+/*
+clang -target bpf -O2 -c ebpf_hello.c -o ebpf_hello.o 
+
+tc qdisc add dev ens3 clsact
+tc filter add dev ens3 ingress bpf obj ebpf_hello.o sec classifier direct-action
+
+tc qdisc show dev ens3
+tc filter show dev ens3 ingress
+
+bpftool prog list  
+*/
+
+
+/*
+clang -target bpf -O0 -c ebpf_hello.c -o ebpf_hello.o 
+tc filter add dev ens3 ingress bpf obj ebpf_hello.o sec classifier direct-action
+
+-O0导致报错:
+
+libbpf: prog 'ebpf_hello': BPF program load failed: Invalid argument
+libbpf: prog 'ebpf_hello': -- BEGIN PROG LOAD LOG --
+unknown opcode 8d
+processed 0 insns (limit 1000000) max_states_per_insn 0 total_states 0 peak_states 0 mark_read 0
+-- END PROG LOAD LOG --
+libbpf: prog 'ebpf_hello': failed to load: -22
+libbpf: failed to load object 'ebpf_hello.o'
+Unable to load program
+*/
