f24f2faa717eb1fa8e22fe9968159ddcc1938427
zzblydia <13098426+zzblydia@users.noreply.github.com>
2025-04-26 17:01:22 +0800

    update python/mijia-api/action-server.py for bug of wifi disconnect

--- python/mijia-api/action-server.py
+++ python/mijia-api/action-server.py
@@ -2,6 +2,7 @@ import socket
 import json
 import subprocess
 import logging
+import ast
 from datetime import datetime, timedelta
 
 SERVER_IP = "192.168.8.62"
@@ -12,16 +13,20 @@ DEVICE_IP = "192.168.8.93"
 DEVICE_DID = "aaa"
 DEVICE_TOKEN = "bbb"
 
-# Shell command template as a global constant
-SHELL_COMMAND_TEMPLATE = (
+SHELL_COMMAND_SET_PROPERTY = (
     "miiocli device --ip {device_ip} --token {device_token} raw_command set_properties "
     '"[{{\'did\': \'{device_did}\', \'siid\': 2, \'piid\': 1, \'value\':{value}}}]"'
+)
+
+SHELL_COMMAND_GET_PROPERTY = (
+    "miiocli device --ip {device_ip} --token {device_token} raw_command get_properties "
+    '"[{{\'did\': \'{device_did}\', \'siid\': 2, \'piid\': 1}}]"'
 )
 
 # 配置日志
 logging.basicConfig(
     level=logging.INFO,  # 设置默认日志级别为 INFO
-    format='[%(asctime)s - %(levelname)s] %(message)s',  # 时间戳 - 级别 - 消息
+    format='[%(asctime)s][%(levelname)s] %(message)s',  # 时间戳 - 级别 - 消息
     handlers=[
         logging.FileHandler('run.log', mode='a'),  # 追加到 run.log
         # logging.StreamHandler()  # 同时输出到控制台
@@ -29,6 +34,8 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
+# nohup python3 -u action-server.py >/dev/null &
 class TCPServer:
     def __init__(self):
         self.ip = SERVER_IP
@@ -41,12 +48,11 @@ class TCPServer:
         self.last_valid_data_time = datetime.now()  # Initialize to start time
         self.current_state = False  # Initialize state to False (off)
 
-    def set_switch_off(self):
-        shell_command = SHELL_COMMAND_TEMPLATE.format(
+    def set_current_state(self):
+        shell_command = SHELL_COMMAND_GET_PROPERTY.format(
             device_ip=self.device_ip,
             device_did=self.device_did,
             device_token=self.device_token,
-            value="False"
         )
         try:
             result = subprocess.run(
@@ -56,19 +62,30 @@ class TCPServer:
                 text=True,
                 check=True
             )
-            logger.info(f"set_switch_off executed successfully response: {result.stdout}")
+            logger.info(f"get_switch_state successfully response: {result.stdout}")
+            # [{'did': 'MYDID', 'siid': 2, 'piid': 1, 'code': 0, 'value': False}]
+            lines = result.stdout.splitlines()
+            if len(lines) >= 2:
+                second_line = lines[1]
+                # json.loads 要求json字符串为双引号
+                # json_data = json.loads(second_line)
+                # value = json_data.get('value')
+                data = ast.literal_eval(second_line)
+                value = data[0]['value']
+                # 使用插座实际的状态更新, 可能有时延
+                self.current_state = value
+                logger.info(f"get_switch_state update current_state {value}")
         except subprocess.CalledProcessError as e:
-            logger.error(f"Error executing set_switch_off: {e.stderr}")
+            logger.error(f"error1 executing get_switch_state : {e.stderr}")
         except Exception as e:
-            logger.error(f"Unexpected error executing set_switch_off: {str(e)}")
-        self.current_state = False
+            logger.error(f"error2 executing get_switch_state : {str(e)}")
 
-    def set_switch_on(self):
-        shell_command = SHELL_COMMAND_TEMPLATE.format(
+    def set_switch_state(self, value):
+        shell_command = SHELL_COMMAND_SET_PROPERTY.format(
             device_ip=self.device_ip,
             device_did=self.device_did,
             device_token=self.device_token,
-            value="True"
+            value=value
         )
         try:
             result = subprocess.run(
@@ -78,12 +95,11 @@ class TCPServer:
                 text=True,
                 check=True
             )
-            logger.info(f"set_switch_on executed successfully: {result.stdout}")
+            logger.info(f"set_switch_state {value} successfully response: {result.stdout}")
         except subprocess.CalledProcessError as e:
-            logger.error(f"Error executing set_switch_on: {e.stderr}")
+            logger.error(f"error executing set_switch_state {value} : {e.stderr}")
         except Exception as e:
-            logger.error(f"Unexpected error executing set_switch_on: {str(e)}")
-        self.current_state = True
+            logger.error(f"error executing set_switch_state {value} : {str(e)}")
 
     def start(self):
         try:
@@ -99,7 +115,7 @@ class TCPServer:
 
                     if data:
                         current_time = datetime.now()
-                        logger.info(f"Received data at {current_time}: {data}")
+                        logger.info(f"Received data {data}")
 
                         try:
                             json_data = json.loads(data)
@@ -107,13 +123,13 @@ class TCPServer:
                             is_charging = json_data.get('IsCharging')
 
                             if percent < BATTERY_PERCENT_MIN and not is_charging:
-                                self.set_switch_on()
+                                self.set_switch_state(True)
+                                self.set_current_state()
                             elif percent > BATTERY_PERCENT_MAX and is_charging:
-                                self.set_switch_off()
-
+                                self.set_switch_state(False)
+                                self.set_current_state()
                             # 如果数据有效则更新数据接收时间
                             self.last_valid_data_time = current_time
-
                         except json.JSONDecodeError:
                             logger.warning("Invalid JSON, continuing to listen...")
                             continue
@@ -123,15 +139,18 @@ class TCPServer:
                     # Timeout occurred, check for 3-minute timeout if state is on
                     if self.current_state:
                         time_diff = datetime.now() - self.last_valid_data_time
-                        # if time_diff > timedelta(seconds=5):
-                        if time_diff > timedelta(minutes=3):
-                            self.set_switch_off()
+                        # 客户端3分钟发一次, 3次没发则认为客户端关机, 需要关闭插座
+                        if time_diff > timedelta(seconds=600):
+                            self.set_switch_state(False)
+                            self.set_current_state()
                             self.last_valid_data_time = datetime.now()  # Reset to current time
 
         except Exception as e:
+            print(f"pServer error: {e}")
             logger.error(f"Server error: {e}")
         finally:
             self.server_socket.close()
+
 
 if __name__ == "__main__":
     server = TCPServer()
