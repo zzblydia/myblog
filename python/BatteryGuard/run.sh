c9219dec5fa815edf069a7dc76df296796130188
zzblydia <13098426+zzblydia@users.noreply.github.com>
2026-04-08 21:55:16 +0800

    repair BatteryGuard run.sh

--- python/BatteryGuard/run.sh
+++ python/BatteryGuard/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+
 source /vol1/python/BatteryGuard/.venv/bin/activate
-nohup python3 /vol1/python/BatteryGuard/battery_guard_single_client.py >/dev/null 2>&1 &
+python3 /vol1/python/BatteryGuard/battery_guard_single_client.py
+
